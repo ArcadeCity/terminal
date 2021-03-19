ea83ecf77203168ca34ea05f79f2925e98b303ad
Christopher David <chris@arcade.city>
2021-03-19 12:14:18 -0500

    Use live Magic key in production

--- src/utilities/magic.js
+++ src/utilities/magic.js
@@ -1,6 +1,8 @@
 import { Magic } from 'magic-sdk'
 
-process.browser && console.log(process.env.NODE_ENV)
+const magicKey =
+  process.env.NODE_ENV === 'production'
+    ? process.env.NEXT_PUBLIC_MAGIC_LIVE_PK
+    : process.env.NEXT_PUBLIC_MAGIC_TEST_PK
 
-export const magic =
-  process.browser && new Magic(process.env.NEXT_PUBLIC_MAGIC_TEST_PK)
+export const magic = process.browser && new Magic(magicKey)
