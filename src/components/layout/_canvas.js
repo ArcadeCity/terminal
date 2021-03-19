9431c165e7cbbae625dc337654ea631d5d5076a1
Christopher David <chris@arcade.city>
2021-03-19 10:56:03 -0500

    Axe perf for now

--- src/components/layout/_canvas.js
+++ src/components/layout/_canvas.js
@@ -1,5 +1,4 @@
 import { Canvas } from 'react-three-fiber'
-import { Perf } from 'r3f-perf'
 import useStore from '@/helpers/store'
 import { OrbitControls, Preload } from '@react-three/drei'
 import { a, useSpring } from '@react-spring/three'
@@ -27,7 +26,6 @@ const LCanvas = ({ children }) => {
       <A11yUserPreferences>
         <Preload all />
         <Bg />
-        <Perf openByDefault trackGPU={true} position={'bottom-right'} />
         <OrbitControls />
         <EffectComposer>
           <Vignette eskil={false} offset={0.1} darkness={1.1} />
