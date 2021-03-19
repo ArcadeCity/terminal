712ccacabf9c2f1495a8d904746cf1708e64ff27
Christopher David <chris@arcade.city>
2021-03-19 11:00:09 -0500

    Mono font

--- src/components/layout/_dom.js
+++ src/components/layout/_dom.js
@@ -14,7 +14,10 @@ const Dom = ({ dom }) => {
   const events = useStore((s) => s.events)
   events && delete events.onGotPointerCaptureLegacy // https://github.com/pmndrs/react-three-fiber/issues/462#issuecomment-653227107
   return (
-    <div className='absolute top-0 left-0 right-0 z-20 dom' {...events}>
+    <div
+      className='font-mono absolute top-0 left-0 right-0 z-20 dom'
+      {...events}
+    >
       <Header />
       {dom}
       <h1 className='absolute w-full text-xs tracking-wider text-center text-gray-100 md:mt-56 mt-28 top-1/2 sm:subpixel-antialiased md:antialiased'>
