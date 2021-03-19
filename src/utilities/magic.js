49f451713a9882688d620a93841af891866cfcc1
Christopher David <chris@arcade.city>
2021-03-19 12:10:56 -0500

    Check node_env

--- src/utilities/magic.js
+++ src/utilities/magic.js
@@ -1,4 +1,6 @@
 import { Magic } from 'magic-sdk'
 
+process.browser && console.log(process.env.NODE_ENV)
+
 export const magic =
   process.browser && new Magic(process.env.NEXT_PUBLIC_MAGIC_TEST_PK)
