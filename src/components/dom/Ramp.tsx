8b8da955e73c6b5c0542f2cda831a786ea6a1b28
Christopher David <chris@arcade.city>
2021-03-23 23:07:03 -0500

    Ramp uses prod key and user info

--- src/components/dom/Ramp.tsx
+++ src/components/dom/Ramp.tsx
@@ -1,15 +1,20 @@
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
+import useStore from '@/helpers/store'
 
 export const Ramp = () => {
+  const magicUser = useStore((s) => s.magicUser)
+  console.log(magicUser)
+  if (!magicUser) return
+
   const buy = () => {
     new RampInstantSDK({
       hostAppName: 'Arcade City',
       hostLogoUrl: 'https://arcade.city/img/icon.png',
       swapAsset: 'ETH',
-      url: 'https://ri-widget-staging.firebaseapp.com',
-      userAddress: '0xa5c249AD3a680523B6aF014Cf737b89032b3f81c',
-      userEmailAddress: 'chris@arcade.city',
-      hostApiKey: process.env.NEXT_PUBLIC_RAMP_STAGING_KEY,
+      userAddress: magicUser.publicAddress,
+      userEmailAddress: magicUser.email,
+      hostApiKey: process.env.NEXT_PUBLIC_RAMP_PRODUCTION_KEY,
+      // url: 'https://ri-widget-staging.firebaseapp.com',
     })
       .on('*', (event) => console.log(event))
       .show()
