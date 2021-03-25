4077cdcdd138893dd1d5eed19f374c3a02ae4af4
Christopher David <chris@arcade.city>
2021-03-25 07:22:26 -0500

    remove loginbox spring for now

--- src/ui/LoginBox.tsx
+++ src/ui/LoginBox.tsx
@@ -19,10 +19,10 @@ export const LoginBox = () => {
   return (
     <a.div
       className='h-screen w-screen flex flex-col justify-center items-center'
-      style={{
-        opacity: spring.x,
-        // color: spring.x.to([0, 1], ['#7fffd4', '#c70f46']),
-      }}
+      // style={{
+      //   opacity: spring.x,
+      //   // color: spring.x.to([0, 1], ['#7fffd4', '#c70f46']),
+      // }}
     >
       <div className='bg-purple bg-opacity-80 p-12 text-center rounded-lg flex flex-col justify-center items-center'>
         {/* <h1>Terminal Login</h1> */}
