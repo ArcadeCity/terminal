881c624de0c91cf913aec5f1d55b992cad4a9b62
Christopher David <chris@arcade.city>
2021-03-25 06:26:24 -0500

    Fade in login box

--- src/ui/LoginBox.tsx
+++ src/ui/LoginBox.tsx
@@ -1,14 +1,34 @@
 import Link from 'next/link'
+import { useSpring } from '@react-spring/core'
+import { a } from '@react-spring/web'
 
 export const LoginBox = () => {
+  const spring = useSpring({
+    from: { x: 0 },
+    to: { x: 1 },
+    config: {
+      mass: 5,
+      tension: 1000,
+      friction: 50,
+      precision: 0.0001,
+      clamp: true,
+    },
+    // loop: { reverse: true },
+  })
   return (
-    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+    <a.div
+      className='h-screen w-screen flex flex-col justify-center items-center'
+      style={{
+        opacity: spring.x,
+        // color: spring.x.to([0, 1], ['#7fffd4', '#c70f46']),
+      }}
+    >
       <div className='bg-purple bg-opacity-80 p-12 text-center rounded-lg flex flex-col justify-center items-center'>
         <h1>Terminal Login</h1>
         <Link href='/box'>
           <button>Dummy login</button>
         </Link>
       </div>
-    </div>
+    </a.div>
   )
 }
