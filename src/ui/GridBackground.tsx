cb9d1f3e6a0a9c6417b762e40188ce927b9a8d77
Christopher David <chris@arcade.city>
2021-03-25 05:08:32 -0500

    GridBackground shows plane with light

--- src/ui/GridBackground.tsx
+++ src/ui/GridBackground.tsx
@@ -1,5 +1,26 @@
+function Plane() {
+  return (
+    <mesh receiveShadow>
+      <planeBufferGeometry attach='geometry' args={[1000, 1000]} />
+      <meshStandardMaterial attach='material' color='#ffffff' />
+    </mesh>
+  )
+}
+
 export const GridBackground = () => {
-  return <h1>Grid Background</h1>
+  return (
+    <>
+      <pointLight position={[-10, -10, 30]} intensity={0.25} />
+      <spotLight
+        intensity={0.3}
+        position={[30, 30, 50]}
+        angle={0.2}
+        penumbra={1}
+        castShadow
+      />
+      <Plane />
+    </>
+  )
 }
 
 export default GridBackground
