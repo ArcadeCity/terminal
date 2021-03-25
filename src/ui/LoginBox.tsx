749a9660879cb6b6f56e7a45212a091dbfde9ad1
Christopher David <chris@arcade.city>
2021-03-25 07:56:00 -0500

    tsignore loginbox type thing

--- src/ui/LoginBox.tsx
+++ src/ui/LoginBox.tsx
@@ -18,12 +18,11 @@ export const LoginBox = () => {
     <a.div
       className='h-screen w-screen flex flex-col justify-center items-center'
       style={{
+        // @ts-ignore
         opacity: spring.x,
-        // color: spring.x.to([0, 1], ['#7fffd4', '#c70f46']),
       }}
     >
       <div className='bg-purple p-12 text-center rounded-lg flex flex-col justify-center items-center'>
-        {/* <h1>Terminal Login</h1> */}
         <button>Dummy login</button>
       </div>
     </a.div>
