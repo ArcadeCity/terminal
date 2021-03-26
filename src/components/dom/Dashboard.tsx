e289d4eac963e623ddfe66bfed0a416ee5079614
Christopher David <chris@arcade.city>
2021-03-26 09:57:28 -0500

    Pull in dashboard components

--- src/components/dom/Dashboard.tsx
+++ src/components/dom/Dashboard.tsx
@@ -1,3 +1,24 @@
+import { ArcadeUI } from '@arcadecity/ui'
+import { Arweave, MagicLogin, Ramp } from '@/components/dom'
+import { Header } from '@/components/layout/_dom'
+import { MyBalances } from '@/components/dom/token/MyBalances'
+import { useStore } from '@/store'
+
 export const Dashboard = () => {
-  return <h1 className='text-white'>Dashboard</h1>
+  const balances = useStore((s) => s.balances)
+  return (
+    <ArcadeUI>
+      <Header />
+      <MagicLogin />
+      <div className='mt-16'>
+        <MyBalances balances={balances} />
+      </div>
+      <Ramp />
+      <Arweave />
+      <p className='mt-8 text-center italic'>
+        This is alpha software. Please do not store large amounts in connected
+        wallets.
+      </p>
+    </ArcadeUI>
+  )
 }
