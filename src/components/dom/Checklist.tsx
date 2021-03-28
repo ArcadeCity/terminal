8a65a72240c3e7c90043fe3800f1c74779b9c83d
Christopher David <chris@arcade.city>
2021-03-28 09:13:03 -0500

    Move checklist to homepage

--- src/components/dom/Checklist.tsx
+++ src/components/dom/Checklist.tsx
@@ -12,46 +12,39 @@ export const Checklist = () => {
 
   const doneClasses = 'opacity-50 line-through'
   return (
-    <ArcadeUI>
-      <Header />
-      <Navbar />
-      <div className='-mt-32 flex flex-col h-screen w-screen justify-center items-center'>
-        <Card title='Onboarding' className='max-w-md '>
-          <List className='mb-0 px-12 space-y-1 text-lg'>
-            <li>
-              <Text>
-                <span className={verifiedEmail ? doneClasses : ''}>
-                  Verify email address
-                </span>
-              </Text>
-            </li>
-            <li>
-              <Text>
-                <span className={ethWalletConnected ? doneClasses : ''}>
-                  Connect ETH wallet
-                </span>
-              </Text>
-            </li>
-            <li>
-              <Text>
-                <span className={arAddress ? doneClasses : ''}>
-                  Connect AR wallet
-                </span>
-              </Text>
-            </li>
-            <li>
-              <Text>Get Arcade Tokens</Text>
-            </li>
-            <li>
-              <Text>Complete intro quest</Text>
-            </li>
-            <li>
-              <Text>Say hello in chat</Text>
-            </li>
-          </List>
-        </Card>
-        {/* <Arweave /> */}
-      </div>
-    </ArcadeUI>
+    <Card title='Onboarding' className='max-w-md '>
+      <List className='mb-0 px-12 space-y-1 text-lg'>
+        <li>
+          <Text>
+            <span className={verifiedEmail ? doneClasses : ''}>
+              Verify email address
+            </span>
+          </Text>
+        </li>
+        <li>
+          <Text>
+            <span className={ethWalletConnected ? doneClasses : ''}>
+              Connect ETH wallet
+            </span>
+          </Text>
+        </li>
+        <li>
+          <Text>
+            <span className={arAddress ? doneClasses : ''}>
+              Connect AR wallet
+            </span>
+          </Text>
+        </li>
+        <li>
+          <Text>Get Arcade Tokens</Text>
+        </li>
+        <li>
+          <Text>Complete intro quest</Text>
+        </li>
+        <li>
+          <Text>Say hello in chat</Text>
+        </li>
+      </List>
+    </Card>
   )
 }
--- src/pages/checklist.tsx
+++ src/pages/checklist.tsx
@@ -1,3 +1,6 @@
+import { ArcadeUI, Card, List, Text } from '@arcadecity/ui'
+import { Arweave, Navbar } from '@/components/dom'
+import { Header } from '@/components/layout/_dom'
 import { ComponentType } from 'react'
 import dynamic from 'next/dynamic'
 import { Checklist } from '@/components/dom/Checklist'
@@ -16,7 +19,18 @@ const Page = () => {
   return (
     <>
       <GridBackground r3f />
-      {magicUser ? <Checklist /> : <LoginBox useStore={useStore} />}
+
+      {magicUser ? (
+        <ArcadeUI>
+          <Header />
+          <Navbar />
+          <div className='-mt-32 flex flex-col h-screen w-screen justify-center items-center'>
+            <Checklist />
+          </div>
+        </ArcadeUI>
+      ) : (
+        <LoginBox useStore={useStore} />
+      )}
     </>
   )
 }
