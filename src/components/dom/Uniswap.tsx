269d3ac4ce79cdebe0c67a1b4cd8fd699b94f242
Christopher David <chris@arcade.city>
2021-03-29 05:31:42 -0500

    Swap explainer

--- src/components/dom/Uniswap.tsx
+++ src/components/dom/Uniswap.tsx
@@ -1,49 +1,74 @@
 import { useState } from 'react'
 import styled from 'styled-components'
 import ReactSlider from 'react-slider'
-import { Card, List, Text } from '@arcadecity/ui'
+import { Button, Card, List, Text } from '@arcadecity/ui'
 import { MyBalances } from '@/components/dom/token/MyBalances'
 import { useStore } from '@/store'
 
 export const Uniswap = () => {
-  const [value, setValue] = useState<number | number[]>(0.1)
+  const [value, setValue] = useState<number>(0.05)
   const magicUser = useStore((s) => s.magicUser)
   const balances = useStore((s) => s.balances)
   // const ethWalletConnected = magicUser && magicUser.publicAddress
   // const hasArcd = balances && balances.ARCD > 0
 
+  const ethInUsd = balances ? Math.round(value * balances.ethPrice) : '--'
+
   return (
     <>
       <MyBalances balances={balances} />
-      <Card title='Swap ETH for ARCD' className='mt-12 max-w-lg'>
+      <Card
+        title='Swap ETH for ARCD'
+        className='mt-12 w-128 text-center flex flex-col items-center justify-center'
+        style={{ width: 500 }}
+      >
+        <Text className='mb-8'>
+          Drag the slider to select how much ETH to swap for ARCD. Trades are
+          made via the ARCD-ETH pool on Uniswap. ETH gas prices are not included
+          in the dollar estimate and could cost ~$50 or more depending on
+          network congestion.
+        </Text>
         <StyledSlider
           value={value}
           onChange={(val) => {
+            if (typeof val !== 'number') return
             setValue(val)
           }}
-          defaultValue={0.3}
-          step={0.1}
-          min={0.1}
-          max={5}
+          defaultValue={0.025}
+          step={0.025}
+          min={0.025}
+          max={1}
           renderTrack={Track}
           renderThumb={Thumb}
         />
+        <Button>
+          Swap {value} ETH {`(~$${ethInUsd})`} for ARCD
+        </Button>
+        <p className='my-4'>or</p>
+        <a
+          href='https://info.uniswap.org/pair/0xa2e10137dbe88a10dd197f24efa403f0036e4f70'
+          target='_blank'
+        >
+          <Button>Trade on Uniswap</Button>
+        </a>
       </Card>
     </>
   )
 }
 
 const StyledSlider = styled(ReactSlider)`
-  width: 100%;
+  width: 400px;
   height: 25px;
+  margin: 0px auto 35px;
 `
 
 const StyledThumb = styled.div`
-  height: 25px;
+  height: 40px;
+  margin-top: -8px;
   line-height: 25px;
-  width: 25px;
+  width: 40px;
   text-align: center;
-  background-color: #000;
+  background-color: #1c133a;
   color: #fff;
   border-radius: 50%;
   cursor: grab;
@@ -51,14 +76,20 @@ const StyledThumb = styled.div`
 `
 
 const Thumb = (props, state) => (
-  <StyledThumb {...props}>{state.valueNow}</StyledThumb>
+  <StyledThumb {...props}>
+    <h6 style={{ marginTop: 6 }}>{state.valueNow}</h6>
+  </StyledThumb>
 )
 
 const StyledTrack = styled.div`
   top: 0;
   bottom: 0;
   background: ${(props) =>
-    props.index === 2 ? '#f00' : props.index === 1 ? '#0f0' : '#ddd'};
+    props.index === 2
+      ? '#f00'
+      : props.index === 1
+      ? 'rgba(0,0,0,0.1)'
+      : '#AE30FF'};
   border-radius: 999px;
 `
 
